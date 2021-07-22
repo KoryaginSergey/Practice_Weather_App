c374540933bfcb1f24394322231dd7c7e45c1682
Alexhan <alexandrganin0@gmail.com>
2021-07-22 12:10:10 +0300

    Linked to test interface

---
+++ Practice_Weather_App/Practice_Weather_App/ViewControllers/CurrentLocationViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CurrentLocationViewController.swift
+//  Practice_Weather_App
+//
+//  Created by Alex Han on 20.07.2021.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+
+    
+    @IBOutlet private weak var currentLocationLabel: UILabel!
+    @IBOutlet private weak var weatherConditionLabel: UILabel!
+    
+    @IBOutlet private weak var temperatureLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setBackgroundImage()
+        getDataFromServer()
+        
+    }
+
+    func getDataFromServer() {
+       
+        Networkmanager.shared.getCurrentWeather(city: "Харьков") { [weak self] current in
+            guard let self = self,
+                  let currentLocation = current?.name,
+                  let weatherConditionsID = current?.weather?[0].id,
+                  let temperature = current?.main?.temp else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                
+                self.currentLocationLabel.text = currentLocation
+                self.weatherConditionLabel.text = WeatherDataSource.weatherIDs[Int(floor(weatherConditionsID))]
+                self.temperatureLabel.text = String(Int(temperature)) + " ºC"
+            }
+        }
+        
+    }
+    func setBackgroundImage() {
+        
+        let background = UIImageView()
+        
+        background.contentMode = .scaleToFill // Or w/e your desired content mode is
+        
+        view.insertSubview(background, at: 0)
+        background.translatesAutoresizingMaskIntoConstraints = false
+        background.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        background.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        background.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        background.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        background.image = UIImage(named: "Mountain")
+    }
+
+}
---
+++ Practice_Weather_App/Practice_Weather_App/WeatherForDaysAheadTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherForDaysAheadTableViewCell.swift
+//  Practice_Weather_App
+//
+//  Created by Alex Han on 21.07.2021.
+//
+
+import UIKit
+
+class WeatherForDaysAheadTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dayOfTheWeekLabel: UILabel!
+    @IBOutlet weak var dayOfTheWeekTemperature: UILabel!
+    @IBOutlet weak var weatherConditionIcon: UIImageView!
+    
+    
+    //MARK: Убрать нил с картинки
+    
+    func setupWeatherForDaysCell(dayOfTheWeek: String, conditionIcon: UIImage?, temperature: String) {
+        dayOfTheWeekLabel.text = dayOfTheWeek
+        weatherConditionIcon.image = conditionIcon
+        dayOfTheWeekTemperature.text = temperature
+    }
+}

f8738ac15ea9b04357ca41dd84fa4e4e9de78c36
Alexhan <alexandrganin0@gmail.com>
2021-07-23 18:34:26 +0300

    Fixed merge conflicts

--- Practice_Weather_App/Practice_Weather_App/ViewControllers/CurrentLocationViewController.swift
+++ Practice_Weather_App/Practice_Weather_App/ViewControllers/CurrentLocationViewController.swift
@@ -11,34 +11,34 @@ import CoreLocation
 class CurrentLocationViewController: UIViewController {
     
     let locationManager = CLLocationManager()
-
+    
     @IBOutlet private weak var currentLocationLabel: UILabel!
     @IBOutlet private weak var weatherConditionLabel: UILabel!
     @IBOutlet private weak var temperatureLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         setBackgroundImage()
         getDataFromServer()
         
     }
     
-
-    private func getDataFromServer() {
-       
-
+    
+    
+    
+    
     //MARK: - функцию настройки местоположения вызывать сдесь
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        startLocationManager()
+        //        startLocationManager()
     }
     
     
-
+    
     func getDataFromServer() {
         
-
+        
         Networkmanager.shared.getCurrentWeather(city: "Харьков") { [weak self] current in
             guard let self = self,
                   let currentLocation = current?.name,
@@ -46,28 +46,22 @@ class CurrentLocationViewController: UIViewController {
                   let temperature = current?.main?.temp else {
                 return
             }
-
+            
             DispatchQueue.main.async {
-
+                
                 self.currentLocationLabel.text = currentLocation
                 self.weatherConditionLabel.text = WeatherDataSource.weatherIDs[Int(floor(weatherConditionsID))]
                 self.temperatureLabel.text = String(Int(temperature)) + " ºC"
+            }
         }
     }
-
+    
+    
     private func setBackgroundImage() {
         
         let background = UIImageView()
-        
-        background.contentMode = .scaleToFill
-        
-
-}
-    func setBackgroundImage() {
-        
-        let background = UIImageView()
         background.contentMode = .scaleToFill // Or w/e your desired content mode is
-
+        
         view.insertSubview(background, at: 0)
         background.translatesAutoresizingMaskIntoConstraints = false
         background.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
@@ -76,86 +70,86 @@ class CurrentLocationViewController: UIViewController {
         background.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         background.image = UIImage(named: "Mountain")
     }
-
-
+    
+    
     func presentForcast() {
-            let weatherForcast = GestureViewController()
+        let weatherForcast = GestureViewController()
         weatherForcast.modalPresentationStyle = .custom
         weatherForcast.transitioningDelegate = self
-            self.present(weatherForcast, animated: true, completion: nil)
-        }
-        
-        @IBAction func didTapPresentForcast(_ sender: Any) {
-            presentForcast()
-        }
+        self.present(weatherForcast, animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapPresentForcast(_ sender: Any) {
+        presentForcast()
+    }
     
 }
 
 extension CurrentLocationViewController: UIViewControllerTransitioningDelegate {
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         PresentationController(presentedViewController: presented, presenting: presenting)
-
-
-}
-
-//MARK: -  locationManager
-private extension CurrentLocationViewController {
-    
-    //MARK: - start settings for cllLocationManager
-    func startLocationManager() {
-        locationManager.requestWhenInUseAuthorization()//запрос положения когда приложение используется
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = 100
-            locationManager.pausesLocationUpdatesAutomatically = false
-            checkAutorisation()
-        }else{
-            self.locationAlert(title: "Геопозиционирование выключено",
-                        message: "разрешить?",
-                            url: URL(string:"App-Prefs:root=LOCATION_SERVICES"))
-        }
-    }
-    
-    
-     func checkAutorisation() {
-        switch CLLocationManager.authorizationStatus() {
-        case .authorizedAlways:
-            break
-        case .authorizedWhenInUse:
-            locationManager.startUpdatingLocation()
-        case .denied:
-            self.locationAlert(title: "Вы запретили использование геопозиции",
-                        message: "разрешить?",
-                            url: URL(string: UIApplication.openSettingsURLString))
-        case .restricted:
-            break
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        default:
-            break
-        }
+        
         
     }
-    
-    //MARK: - alert "go to location settings"
-    func locationAlert(title: String ,message: String,url: URL?) {
-      let alert = UIAlertController(title: title, message: message , preferredStyle:.alert)
+}
+    //MARK: -  locationManager
+    private extension CurrentLocationViewController {
+        
+        //MARK: - start settings for cllLocationManager
+        func startLocationManager() {
+            locationManager.requestWhenInUseAuthorization()//запрос положения когда приложение используется
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager.delegate = self
+                locationManager.desiredAccuracy = 100
+                locationManager.pausesLocationUpdatesAutomatically = false
+                checkAutorisation()
+            }else{
+                self.locationAlert(title: "Геопозиционирование выключено",
+                                   message: "разрешить?",
+                                   url: URL(string:"App-Prefs:root=LOCATION_SERVICES"))
+            }
+        }
+        
+        
+        func checkAutorisation() {
+            switch CLLocationManager.authorizationStatus() {
+                case .authorizedAlways:
+                    break
+                case .authorizedWhenInUse:
+                    locationManager.startUpdatingLocation()
+                case .denied:
+                    self.locationAlert(title: "Вы запретили использование геопозиции",
+                                       message: "разрешить?",
+                                       url: URL(string: UIApplication.openSettingsURLString))
+                case .restricted:
+                    break
+                case .notDetermined:
+                    locationManager.requestWhenInUseAuthorization()
+                default:
+                    break
+            }
+            
+        }
+        
+        //MARK: - alert "go to location settings"
+        func locationAlert(title: String ,message: String,url: URL?) {
+            let alert = UIAlertController(title: title, message: message , preferredStyle:.alert)
             alert.addAction(.init(title: title, style: .default, handler: { alert in
                 if let url = url{
-                            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                        }
-                    }))
-                alert.addAction(.init(title: "отмена", style: .cancel, handler: nil))
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+            }))
+            alert.addAction(.init(title: "отмена", style: .cancel, handler: nil))
             present(alert, animated: true, completion: nil)
+        }
     }
-}
 
 
 
 //MARK: - CLLocationManagerDelegate
 extension CurrentLocationViewController: CLLocationManagerDelegate {
     
-  //MARK: - request of coordinate when changing location
+    //MARK: - request of coordinate when changing location
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let lastLocation = locations.last {
             print(lastLocation.coordinate.latitude , lastLocation.coordinate.longitude)
@@ -178,5 +172,4 @@ extension CurrentLocationViewController: CLLocationManagerDelegate {
         }
     }
 }
-
 

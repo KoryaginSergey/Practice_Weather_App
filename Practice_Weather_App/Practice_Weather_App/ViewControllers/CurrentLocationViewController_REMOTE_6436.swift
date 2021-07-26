5a3beb1b47f195f64f95c720ac3f4d977d58dcec
fedir72 <fedir72@ua.fm>
2021-07-26 17:27:52 +0300

    fix conflict

---
+++ Practice_Weather_App/Practice_Weather_App/ViewControllers/CurrentLocationViewController_REMOTE_6436.swift
@@ -0,0 +1,150 @@
+//
+//  CurrentLocationViewController.swift
+//  Practice_Weather_App
+//
+//  Created by Alex Han on 20.07.2021.
+//
+
+import UIKit
+import CoreLocation
+
+class CurrentLocationViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
+
+    @IBOutlet private weak var currentLocationLabel: UILabel!
+    @IBOutlet private weak var weatherConditionLabel: UILabel!
+    @IBOutlet private weak var temperatureLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        setBackgroundImage()
+        getDataFromServer()
+       // startLocationManager()
+    }
+    
+    //MARK: - функцию настройки местоположения вызывать сдесь
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+       startLocationManager()
+    }
+    
+    
+
+    func getDataFromServer() {
+        
+        Networkmanager.shared.getCurrentWeather(city: "Харьков") { [weak self] current in
+            guard let self = self,
+                  let currentLocation = current?.name,
+                  let weatherConditionsID = current?.weather?[0].id,
+                  let temperature = current?.main?.temp else {
+                return
+            }
+
+            DispatchQueue.main.async {
+
+                self.currentLocationLabel.text = currentLocation
+                self.weatherConditionLabel.text = WeatherDataSource.weatherIDs[Int(floor(weatherConditionsID))]
+                self.temperatureLabel.text = String(Int(temperature)) + " ºC"
+        }
+    }
+}
+    func setBackgroundImage() {
+        
+        let background = UIImageView()
+        background.contentMode = .scaleToFill // Or w/e your desired content mode is
+        view.insertSubview(background, at: 0)
+        background.translatesAutoresizingMaskIntoConstraints = false
+        background.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        background.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        background.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        background.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        background.image = UIImage(named: "Mountain")
+    }
+
+
+}
+
+//MARK: -  locationManager
+extension CurrentLocationViewController {
+    
+    //MARK: - start settings for cllLocationManager
+    func startLocationManager() {
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = 1000
+            locationManager.pausesLocationUpdatesAutomatically = false
+            locationManager.startUpdatingLocation()
+            checkAutorisation()
+            
+        }
+    }
+    
+    
+     func checkAutorisation() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways:
+            break
+        case .authorizedWhenInUse:
+             locationManager.startUpdatingLocation()
+        case .denied:
+            self.locationAlert(title: "Вы запретили использование геопозиции",
+                        message: "разрешить?",
+                            url: URL(string: UIApplication.openSettingsURLString))
+        case .restricted:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
+        }
+
+    }
+    
+    //MARK: - alert "go to location settings"
+    func locationAlert(title: String ,message: String,url: URL?) {
+      let alert = UIAlertController(title: title, message: message , preferredStyle:.alert)
+            alert.addAction(.init(title: title, style: .default, handler: { alert in
+                if let url = url{
+                            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                        }
+                    }))
+                alert.addAction(.init(title: "отмена", style: .cancel, handler: nil))
+            present(alert, animated: true, completion: nil)
+    }
+}
+
+
+
+//MARK: - CLLocationManagerDelegate
+extension CurrentLocationViewController: CLLocationManagerDelegate {
+    
+  //MARK: - request of coordinate when changing location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let lastLocation = locations.last {
+            print(lastLocation.coordinate.latitude , lastLocation.coordinate.longitude)
+            let lat = lastLocation.coordinate.latitude
+            let lon = lastLocation.coordinate.longitude
+            
+            Networkmanager.shared.getCurrentWeatherByLocation(lat: lat, lon: lon) { [weak self] current in
+                guard let self = self,
+                      let currentLocation = current?.name,
+                      let weatherConditionsID = current?.weather?[0].id,
+                      let temperature = current?.main?.temp else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.currentLocationLabel.text = currentLocation
+                    self.weatherConditionLabel.text = WeatherDataSource.weatherIDs[Int(floor(weatherConditionsID))]
+                    self.temperatureLabel.text = String(Int(temperature)) + " ºC"
+                    self.locationManager.stopUpdatingLocation()
+                }
+            }
+        }
+    }
+}
+
+
